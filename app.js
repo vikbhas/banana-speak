b4514e7eb83101d88c0d936ecab77b2ad641fa88
vikbhas <waraa.vignesh@gmail.com>
2020-12-07 19:16:21 +0530

    Fix: App js

--- app.js
+++ app.js
@@ -4,29 +4,25 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-// var serverURL = "https://api.funtranslations.com/translate/minion.json"
-
 
 function getTranslationURL(input) {
-    return serverURL + "?" + "text=" + input
+    return serverURL + "?text=" + input
 }
 
 function errorHandler(error) {
-    console.log("error occured", error);
-    alert("something wrong with server! try again after some time")
+    console.log("ERROR", error);
 }
 
 
 function clickHandler() {
-    console.log(txtInput)
-    var inputText = txtInput.value; // taking input
+    // console.log(txtInput)
+    var inputText = txtInput.value;
 
-    // calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
+            outputDiv.innerText = translatedText;
            })
         .catch(errorHandler)
 };
